3f2d4018e2b76425abd0832128b00f6e56b50439
Manujademin <137299721+Manujademin@users.noreply.github.com>
2023-08-29 11:17:56 -0700

    Add files via upload

---
+++ projectlogin.py
@@ -0,0 +1,22 @@
+#username = manuja
+#password = 2349@m
+
+username = input("Enter youre username :")
+
+password = input("Enter Password ")
+
+if username == "Manuja":
+    print("Username is Correct ")
+else:
+    print("Username is incorrect ")
+
+if password == "2349@m":
+    print("password is correct")
+else:
+    print("Password is incorrect")
+
+if username == "Manuja" and password == "2349@m":
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DINIDE")
+
